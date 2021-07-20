c4bfbed369eedf743c85c7b374e312f8e1b1df19
Nitish Sati <nitishsati8@gmail.com>
2021-07-20 13:19:53 +0530

    controller added

--- swager-ui-demo/src/main/java/com/swagger/demo/controller/DemoController.java
+++ swager-ui-demo/src/main/java/com/swagger/demo/controller/DemoController.java
@@ -1,7 +1,9 @@
 package com.swagger.demo.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @RequestMapping("/api/v0/")
 public class DemoController {
 
