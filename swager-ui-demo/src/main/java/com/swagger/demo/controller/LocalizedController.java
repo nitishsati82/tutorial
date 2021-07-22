b509a8587f71ace99eae75d61cb46814f4884177
Nitish Sati <nitishsati8@gmail.com>
2021-07-22 12:23:55 +0530

    endpoint added

--- swager-ui-demo/src/main/java/com/swagger/demo/controller/LocalizedController.java
+++ swager-ui-demo/src/main/java/com/swagger/demo/controller/LocalizedController.java
@@ -4,6 +4,7 @@ import java.util.Locale;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,9 +17,15 @@ public class LocalizedController {
 	@Autowired
 	MessageSource source;
 
-	@GetMapping(path = "/messages")
+	@GetMapping(path = "v0/messages")
 	public String getLocalizedMessage(@RequestHeader(name = "Accept-Language", required = false) Locale locale) {
 
 		return source.getMessage("message.hello", null, "Default message", locale);
+	}
+
+	@GetMapping(path = "v1/messages")
+	public String getLocalizedMessages() {
+
+		return source.getMessage("message.hello", null, "Default message", LocaleContextHolder.getLocale());
 	}
 }
