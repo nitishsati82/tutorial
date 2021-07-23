aacdae171895a15f5d9d9db2003279add28bc148
Nitish Sati <nitishsati8@gmail.com>
2021-07-23 15:05:24 +0530

    hal project

---
+++ hal-demo/src/main/java/com/hal/demo/HalDemoApplication.java
@@ -0,0 +1,13 @@
+package com.hal.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HalDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HalDemoApplication.class, args);
+	}
+
+}
---
+++ hal-demo/src/test/java/com/hal/demo/HalDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hal.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HalDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
